2020dc2c3e5a2f3e990ba88edb1390b7ca25cb54
SakshamBhatt07 <bhattsaksham2007@gmail.com>
2025-10-15 13:38:34 +0530

    Program to display sum of 2 num.

---
+++ Q1-Q10/Q1.c
@@ -0,0 +1,9 @@
+// Q1: Write a program to input two numbers and display their sum.
+void main()
+{
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum: %d\n", sum);
+}
